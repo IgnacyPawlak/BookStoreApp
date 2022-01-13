651887d44685b142aa8ac077baad52ac76c9372f
nanu-daxx <dagmara.w98@gmail.com>
2022-01-13 14:05:29 +0100

    home page update

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-note',
+    templateUrl: './home.component.html',
+    styles: [
+        
+    ]
+})
+
+export class HomeComponent {
+
+}
